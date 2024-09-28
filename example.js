39770efada40a51d7fdb7a78a1829e53c577dc3a
Fakqr <realprogamer6262@gmail.com>
2024-09-28 19:31:12 -0400

    Update

--- example.js
+++ example.js
@@ -1,7 +1,8 @@
 (function() {
     var script = document.createElement('script');
     script.src = 'https://raw.githubusercontent.com/Fakqr/ModMenuAPI/refs/heads/main/ModMenuAPI.js';
-
+    document.head.appendChild(script);
+    
     var ModMenu = createModMenu('Example', 'This is a Mod Menu!', 'You');
     var Tab01 = createTab(ModMenu, 'Tab 1');
     var Button01 = createButton(Tab01, 'Button', function() {
