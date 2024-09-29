a83ad6c1320341cfa45a9344aa0d14882d91e0f6
Fakqr <realprogamer6262@gmail.com>
2024-09-28 20:18:47 -0400

    Update

--- example.js
+++ example.js
@@ -1,18 +1,35 @@
 (function () {
+    // Loads ModMenu API
     var x = document.createElement('script');
     x.src = 'https://cdn.jsdelivr.net/gh/Fakqr/ModMenuAPI/ModMenuAPI.js';
 
+    // Waits until ModMenu API if fully loaded before continuing
     x.onload = function() {
+        // Create ModMenu
         var ModMenu = createModMenu('Example', 'This is a Mod Menu!', 'You');
+        // Create your first Tab
         var Tab01 = createTab(ModMenu, 'Tab 1');
-        var Button01 = createButton(Tab01, 'Button', function () {
-            console.log('The button was clicked!')
+        // Create your first Button
+        var Button01 = createButton(Tab01 /* Put Tab Variable Here */, 'Button', function () {
+            console.log('The button was clicked!');
         });
+
+        /* 
+        Another method to do button:
+
+        function buttonClicked01() {
+            console.log('The button was clicked!');
+        };
+
+        var Button01 = createButton(Tab01, 'Button', buttonClicked01);
+        */
     };
 
+    // Error Handling
     x.onerror = function(error) {
         console.error('Error while loading script:', error);
     };
 
+    // Loads ModMenu API on to Document.head
     document.head.appendChild(x);
 })();
