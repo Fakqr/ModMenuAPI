105ae1c2c695276a6d292daa187a4a6241ff44ea
Fakqr <realprogamer6262@gmail.com>
2024-09-28 20:07:21 -0400

    Update

--- example.js
+++ example.js
@@ -1,14 +1,18 @@
 (function () {
-    var script = document.createElement('script');
-    script.src = 'https://raw.githubusercontent.com/Fakqr/ModMenuAPI/refs/heads/main/ModMenuAPI.js';
+    var x = document.createElement('script');
+    x.src = 'https://raw.githubusercontent.com/Fakqr/ModMenuAPI/refs/heads/main/ModMenuAPI.js';
 
-    script.onload = function () {
+    x.onload = function() {
         var ModMenu = createModMenu('Example', 'This is a Mod Menu!', 'You');
         var Tab01 = createTab(ModMenu, 'Tab 1');
         var Button01 = createButton(Tab01, 'Button', function () {
             console.log('The button was clicked!')
         });
     };
-    
-    document.head.appendChild(script);
+
+    x.onerror = function(error) {
+        console.error('Error while loading script:', error);
+    };
+
+    document.head.appendChild(x);
 })();
